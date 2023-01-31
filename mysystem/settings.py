0cd902ef2598561f8b578f592a216b30e9ed1868
oswald-pro <oswaldmboussa1@gmail.com>
2023-01-31 19:03:37 +0100

    commit

--- mysystem/settings.py
+++ mysystem/settings.py
@@ -74,7 +74,12 @@ WSGI_APPLICATION = "mysystem.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {}
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 
 # Password validation
